0c5b00a2f21fed2e8d6bc6d4acd70bcb20ab7f88
Sandro Suqnidze <sandrosuqnidze2@gmail.com>
2023-09-10 10:30:15 +0400

    if statement

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (7 < 6) {
+            System.out.println(false);
+        } else {
+            System.out.println(false);
+        }
     }
 }
